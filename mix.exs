6ff22531a5a7373dc106f8207cd4ef45df9c74da
Arvid Kahl <arvid.kahl@digimondo.de>
2016-08-17 16:32:44 +0200

    added files to package metadata

--- mix.exs
+++ mix.exs
@@ -13,8 +13,7 @@ defmodule Sizeable.Mixfile do
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+
   def application do
     [applications: [:logger]]
   end
@@ -24,18 +23,10 @@ defmodule Sizeable.Mixfile do
       maintainers: ["Arvid Kahl"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/arvidkahl/sizeable"},
+      files: ["lib", "test", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],      
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
