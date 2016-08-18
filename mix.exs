75eb3ce6957c88fcc15f6a62a412422350ed6d1d
Arvid Kahl <arvid.kahl@digimondo.de>
2016-08-18 11:43:02 +0200

    bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sizeable.Mixfile do
 
   def project do
     [app: :sizeable,
-     version: "0.1.3",
+     version: "0.1.4",
      elixir: "~> 1.1",
      description: "An Elixir library to make file sizes human-readable.",
      package: package(),
